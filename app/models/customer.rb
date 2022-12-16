f0e57c7bcb389ccf668350d4a53bd635a1117f84
shioriko25 <m.kmkmkaassfh.12@gmail.com>
2022-12-16 06:28:04 +0000

    [add]1:N

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,4 +4,9 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
+  has_many :addresses, dependent: :destroy 
+  has_many :orders, dependent: :destroy
+  has_many :cart_items, dependent: :destroy
+         
+         
 end

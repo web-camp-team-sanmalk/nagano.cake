03aa7f49ef37943f1d93feeb19dc5404e04508f7
mthskzk <116724950+mthskzk@users.noreply.github.com>
2022-12-16 05:42:17 +0000

    [Finish]controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,67 @@
 Rails.application.routes.draw do
+
+  root to: "public/homes#top"
+
   devise_for :admins
   devise_for :customers
   devise_for :users
+
+  namespace :admin do
+    get 'orders/show'
+  end
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/edit'
+  end
+  namespace :admin do
+    get 'items/index'
+    get 'items/new'
+    get 'items/show'
+    get 'items/edit'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :admin do
+    get 'sessions/new'
+  end
+  namespace :public do
+    get 'addresses/index'
+    get 'addresses/edit'
+  end
+  namespace :public do
+    get 'orders/new'
+    get 'orders/about'
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'cart_items/index'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/unsubscribe'
+  end
+  namespace :public do
+    get 'sessions/new'
+  end
+  namespace :public do
+    get 'registrations/new'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

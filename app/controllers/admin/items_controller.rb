afbc0d640537ab96c0f489721dfffa6abdc97fca
shioriko25 <m.kmkmkaassfh.12@gmail.com>
2022-12-17 13:05:08 +0000

    [add]商品詳細画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -9,12 +9,19 @@ class Admin::ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
   end
 
   def create
+    @item = Item.new(item_params)
+    if  @item.save
+      redirect_to admin_items_path(@item.id)
+    else
+      render :new
+    end
   end
 
   def update
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,5 @@ class Item < ApplicationRecord
   has_one_attached :image
   
   
+  
 end

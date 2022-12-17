c9adde8f25c0513d20c43599b5d3064681237b9f
ryota0603 <114867300+ryota0603@users.noreply.github.com>
2022-12-17 05:19:46 +0000

    contoroler.admin,public,devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,18 @@ Rails.application.routes.draw do
 
   root to: "public/homes#top"
 
-  devise_for :admins
-  devise_for :customers
+  # 顧客用
+# URL /customers/sign_in ...
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
 
   namespace :admin do
     get 'homes/top'

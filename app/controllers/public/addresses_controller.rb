1dd2b3363ca2e013033b5b255a2cfcb198e1a696
mthskzk <116724950+mthskzk@users.noreply.github.com>
2022-12-18 07:36:32 +0000

    [追加]配送先

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,5 +1,11 @@
 class Public::AddressesController < ApplicationController
+
+  before_action :ensure_correct_customer
+
   def index
+    @customer = current_customer
+    @addresses = @customer.addresses
+    @address = Addresses.new
   end
 
   def edit
@@ -12,5 +18,19 @@ class Public::AddressesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:postal_code, :address, :name)
+  end
+
+  # ログインしているユーザーの住所編集画面かの判断
+  def ensure_correct_customer
+    @customer = Customer.find(params[:id])
+    unless @customer == current_customer
+      redirect_to customer_path(current_customer)
+    end
   end
 end

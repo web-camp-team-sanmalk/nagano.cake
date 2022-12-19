d1b7e811e8b5d0b3435662bca5d531ef44bd2908
MoekaKakimoto <k.moeka0984@gmail.com>
2022-12-19 08:01:55 +0000

    [実装]admin_orders

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,14 @@
 class Admin::HomesController < ApplicationController
+  
   def top
+    if params[:customer_id]
+      #遷移してきたIDをカスタマーIDに入れて、whereで取得
+      @orders = Order.where(customer_id: params[:customer_id])
+    else
+      #オーダーのデーター全部
+      @orders = Order.page(params[:page]).per(10)
+    end
+    @orders_page = Order.page(params[:page]).per(10)
   end
+  
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,24 @@
 class Admin::OrdersController < ApplicationController
+  before_action :authenticate_admin!
+
   def show
+    @order = Order.find(params[:id])
+    @order_items = @order.order_items
   end
 
   def update
+    order = Order.find(params[:id])
+    order.update(order_params)
+    order.order_item_status_auto_update
+    redirect_to admin_order_path(params[:id])
   end
+
+
+  private
+
+
+  def order_params
+    params.require(:order).permit(:buy_status)
+  end
+
 end

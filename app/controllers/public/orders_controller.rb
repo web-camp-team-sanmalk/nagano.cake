e3977f17c49bc363d6447f1f526df678a881824c
mthskzk <116724950+mthskzk@users.noreply.github.com>
2022-12-20 11:27:04 +0000

    [追加]confirmアクション

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -13,13 +13,30 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    @cart_items = current_customer.cart_items.all
+    # 自分の住所を選択した場合
+    if params[:order][:address_number] == "1"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.last_name + current_customer.first_name
+    # 登録済みの住所を選択した場合
+    elsif params[:order][:address_number] == "2"
+      @address = Address.find(params[:order][:address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+    end
   end
 
   def create
   end
 
   private
+
   def order_params
-    params.require()
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
+  end
 
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -2,19 +2,12 @@ class Address < ApplicationRecord
 
   belongs_to :customer
 
-<<<<<<< HEAD
-
-
-
-
-
- def address_display
-    '〒'+ postal_code + ' ' + address + ' ' + name
-  end
-=======
   validates :postal_code, presence: true, length: { is: 7 }
   validates :address, presence: true
   validates :name, presence: true
->>>>>>> origin/develop
+
+  def address_display
+    '〒' + postal_code + ' ' + address + ' ' + name
+  end
 
 end

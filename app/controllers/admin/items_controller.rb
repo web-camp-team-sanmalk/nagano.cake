d1dabed65cac653777108db818fffcabc4921e57
shioriko25 <m.kmkmkaassfh.12@gmail.com>
2022-12-20 03:38:54 +0000

    [add]商品バリデーション

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -26,7 +26,7 @@ class Admin::ItemsController < ApplicationController
     @item = Item.new(item_params)
     if  @item.save
       redirect_to admin_item_path(@item.id)
-      flash[:notice] = "成功しました"
+      flash[:notice] = "商品が追加されました"
     else
       render :new
     end
@@ -36,7 +36,7 @@ class Admin::ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
-       flash[:notice] = "成功しました"
+       flash[:notice] = "商品を変更しました"
        redirect_to admin_item_path(@item.id)
     else
        render :edit
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,14 @@ class Item < ApplicationRecord
 
   has_one_attached :image
 
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true
+  validates :genre_id, presence: true
+  validates :is_active, inclusion: { in: [true, false] }
+
+
 def  add_tax_price
   (self.price * 1.10).round
 end

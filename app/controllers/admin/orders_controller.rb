738cc7ea72b43f492637099796739a19a13307d6
MoekaKakimoto <k.moeka0984@gmail.com>
2022-12-21 09:30:00 +0000

    [Add]admin_orders

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,22 +3,20 @@ class Admin::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
-    @order_items = @order.order_items
+    @order_details = @order.order_details.all
   end
 
   def update
     order = Order.find(params[:id])
     order.update(order_params)
-    order.order_item_status_auto_update
+    order.order_details_status_auto_update
     redirect_to admin_order_path(params[:id])
   end
 
-
   private
 
-
   def order_params
-    params.require(:order).permit(:buy_status)
+    params.require(:order).permit(:status)
   end
 
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -3,19 +3,19 @@ class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :item
 
-  enum make_status: {着手不可:0, 製作待ち:1,  製作中:2, 製作完了:3}
+  enum production_status: {着手不可:0, 製作待ち:1,  製作中:2, 製作完了:3}
 
   def production_status_auto_update
-    if self.make_status == "製作中"
-      self.order.update(buy_status: "製作中")
+    if self.production_status == "製作中"
+      self.order.update(status: "製作中")
     end
   end
 
   def status_auto_update
-    order_items = self.order.order_items
-    make_status = order_items.pluck(:make_status)
-     if make_status.all?{ |make_status| make_status == "製作完了"}
-       self.order.update(buy_status: "発送準備中")
+    order_details = self.order.order_details
+    production_status = order_details.pluck(:production_status)
+     if production_status.all?{ |production_status| production_status == "製作完了"}
+       self.order.update(status: "発送準備中")
      end
   end
 

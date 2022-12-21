50bd503981bb3a8b50cf7f4ca73193a4916dc622
shioriko25 <m.kmkmkaassfh.12@gmail.com>
2022-12-21 03:29:35 +0000

    [add]カート機能

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -13,12 +13,12 @@ class Public::CartItemsController < ApplicationController
     @cart_item = CartItem.find(params[:id])
     @cart_item.destroy
     redirect_to cart_items_path
-  end
+   end
 
   def destroy_all
     @cart_items = current_customer.cart_items.all
     @cart_items.destroy_all
-
+     redirect_to cart_items_path
   end
 
 

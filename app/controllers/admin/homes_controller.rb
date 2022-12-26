45316c78b67431d4f0a8f4848803b10d5af1cc52
mthskzk <116724950+mthskzk@users.noreply.github.com>
2022-12-26 07:21:17 +0000

    エラー解決

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,9 +1,9 @@
 class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
   def top
-    if params[:customer_id]
+    unless params[:customer_id].nil?
       #遷移してきたIDをカスタマーIDに入れて、whereで取得
-      @orders = Order.where(customer_id: params[:customer_id])
+      @orders = Order.where(customer_id: params[:customer_id]).page(params[:page]).per(10)
       @customer = Customer.find(params[:customer_id])
       @num =1
     else
@@ -11,11 +11,6 @@ class Admin::HomesController < ApplicationController
       @orders = Order.page(params[:page]).per(10)
       @num =2
     end
-     @orders_page = Order.page(params[:page]).per(10)
   end
-  
-  def top
-    @orders = Order.page(params[:page])
-  end
-  
+
 end
